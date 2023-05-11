5e4212a1a2550f4c65c6b79117a5b4ffaa10ea66
Stanislav Grokhotov <stanislav.skii2000@mail.ru>
2023-05-11 18:03:32 +0700

    almost learn 4 lesson

--- src/main/kotlin/ru/youngstanis/kotlinbasics/Test.kt
+++
@@ -1,8 +0,0 @@
-package ru.youngstanis.kotlinbasics
-
-import ru.youngstanis.kotlinlang.basics.sum;
-
-fun main(args: Array<String>) {
-    println("Hello my test")
-    println(sum(15, -30))
-}
---
+++ src/main/kotlin/ru/youngstanis/kotlinbasics/operators/Operators.kt
@@ -0,0 +1,46 @@
+package ru.youngstanis.kotlinbasics.operators
+
+fun main(args: Array<String>) {
+
+    println(
+        """
+        - Lesson 4
+        |-- Logic and comparison operators 
+
+    """.trimIndent()
+    )
+
+    val a = 1 + 1
+    val b = a == 1
+
+    println(a)
+    println("${b}\n")
+
+    // <, >, <=, >=, !=, ==
+    // true
+    // false
+
+    val currentGrade = "Middle"
+    val comparisonResult = templateGrade == currentGrade
+
+    println("""
+        - Are you middle backend dev?
+        - ${if(comparisonResult == true) "Yes, I'm middle" else "No, I'm ${currentGrade}"}
+    """.trimIndent())
+
+    // &&, ||, !
+
+    val c = false
+    val d = !c
+
+    println(d)
+
+    val age = 22
+
+    // val result = (currentGrade == "Middle" || currentGrade == "Senior") || (age >= 27 && age <= 70)
+    // val result = (currentGrade == "Middle" || currentGrade == "Senior") or (age >= 27 && age <= 70)
+    val result = (currentGrade == "Middle" || currentGrade == "Senior") and (age in 22..70)
+    println("- Can you be a lead? $result")
+}
+
+const val templateGrade: String = "Middle"
