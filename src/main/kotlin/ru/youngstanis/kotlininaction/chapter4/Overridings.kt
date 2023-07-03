9f9f83d5e8aeccaf9e029647a39f14e6cb20a49a
Stanislav Grokhotov <stanislav.skii2000@mail.ru>
2023-07-04 01:37:42 +0700

    learned about overridings

--- src/main/kotlin/ru/youngstanis/kotlininaction/chapter4/Overridings.kt
+++ src/main/kotlin/ru/youngstanis/kotlininaction/chapter4/Overridings.kt
@@ -6,9 +6,30 @@ open class Human(
 
 class Client(
     val name: String,
-    val postalCode: Int
-): Human() {
+    val postalCode: Int,
+    val _age: Int
+): Human(_age) {
 
-    override fun toString(): String = "Client(name = $name, postalCode= $postalCode)"
+    override fun equals(other: Any?): Boolean {
+        if(other == null || other !is Client) {
+            return false
+        }
+        return this.name == other.name
+            && this.postalCode == other.postalCode
+            && this.age == other.age
+    }
 
+    override fun toString(): String = "Client(name = $name, postalCode = $postalCode)"
+
+    override fun hashCode(): Int = name.hashCode() * 31 + age + postalCode
+}
+
+fun main(args: Array<String>) {
+    val client1 = Client("Stas", 630061, 23)
+    val client2 = Client("Stas", 630061, 23)
+    val client3 = Client("Kolya", 234654, 27)
+
+    println(client3.toString())
+    println(client1 == client2)
+    println(client3 == client2)
 }
